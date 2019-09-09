ce523f8fadbd3c262f91ce261761338fae459774
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-09 17:52:36 -0400

    completes buttons to increment bottom row

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,6 +7,8 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [lionsScore, setLionsScore] = useState(0);
 const [tigersScore, setTigersScore] = useState(0);
+
+
 
   return (
     <div className="container">
@@ -19,7 +21,7 @@ const [tigersScore, setTigersScore] = useState(0);
 
             <div className="home__score">{lionsScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">0:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{tigersScore}</div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+// set initial state values
+const [down, setDown] = useState(0);
+const [toGo, setToGo] = useState(4);
+const [ballOn, setBallOn] = useState(0);
+const [quarter, setQuarter] = useState(1);
+
   return (
+    
     <div className="bottomRow">
       <div className="down">
-        <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <button className="down__title" onClick={() => setDown(down + 1)}>Down</button>
+        <div className="down__value">{down}</div>
       </div>
       <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <button className="toGo__title" onClick={() => {
+          if (toGo > 1) {
+            setToGo(toGo - 1)
+          } else if (toGo === 1) {
+            setToGo(4)
+          }
+        }}>To Go</button>
+        <div className="toGo__value">{toGo}</div>
       </div>
       <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <button className="ballOn__title" onClick={() => setBallOn(ballOn + 1)}>Ball on</button>
+        <div className="ballOn__value">{ballOn}</div>
       </div>
       <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <button className="quarter__title" onClick={() => {
+          if (quarter < 4) {
+            setQuarter(quarter + 1);
+          } else if (quarter === 4) {
+            setQuarter(1)
+          }
+        }}>Quarter</button>
+        <div className="quarter__value">{quarter}</div>
       </div>
     </div>
   );
